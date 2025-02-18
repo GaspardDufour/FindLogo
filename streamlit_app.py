5d24dde73fb0dfcc04f12d5f5c05d9590d764d71
GASPARD <gaspard.dufour@esme.fr>
2025-02-18 15:39:03 +0000

    edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from ultralytics import YOLO
 import tempfile
 
 # Charger le modèle YOLO
-model = YOLO("yolo11n.pt")  # Remplace par ton modèle
+model = YOLO("yolo11n.pt")  # Remplace par le chemin vers ton modèle
 
 st.title("Détection de Logo avec YOLO")
 
@@ -18,12 +18,10 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
 
     # Convertir en RGB si nécessaire
-    if image.mode == "P":
-        image = image.convert("RGBA")
-    else:
+    if image.mode != "RGB":
         image = image.convert("RGB")
 
-    image_np = np.array(image)  # Convertir en numpy array pour OpenCV
+    image_np = np.array(image)
 
     # Sauvegarde temporaire
     with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp_file:
@@ -32,26 +30,22 @@ if uploaded_file is not None:
 
     # Exécuter YOLO
     results = model(image_path)
-
-    detected_classes = []  # Liste des classes détectées
-
-    # Dessiner les bounding boxes
+    detected_classes = []
+    # Dessiner les boîtes et écrire le numéro de la classe
     for result in results:
         for box in result.boxes.data:
-            x1, y1, x2, y2, _, cls = box.tolist()
-            cls = int(cls)
-            detected_classes.append(cls)
-
-            # Dessiner la bounding box
+            x1, y1, x2, y2, conf, cls = box.tolist()
+            cls = int(cls)  # Convertir en entier
+            
+            # Dessiner la boîte et écrire le numéro de classe
             cv2.rectangle(image_np, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 3)
+            detected_classes.append(int(cls))
 
-    # Afficher l'image avec les bounding boxes
+    # Afficher l'image avec les détections
     st.image(image_np, caption="Résultat de la détection", use_container_width=True)
-
-    # Afficher les classes détectées
     if detected_classes:
-        st.write("### Classes détectées :")
-        for cls in detected_classes:
-            st.write(f"➡️ Classe détectée : **{cls}**")
-    else:
-        st.write("Aucune classe détectée.")
+            st.write("### Classes détectées :")
+            for cls in detected_classes:
+                st.write(f"➡️ Classe détectée : **{cls}**")
+        else:
+            st.write("Aucune classe détectée.")
